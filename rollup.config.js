7a86aa204bf0d43bde9b0eaffcb6066199088204
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-01-23 15:52:18 +0100

    Use named exports setting in Rollup for CJS bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'es' },
   ],
   plugins: [babel()],
