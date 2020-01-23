a8377ad373f3f312f23f33bf83ea06a74cb335e9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-01-23 15:56:24 +0100

    Add .name to the returned plugin function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-export default function extraScopePlugin(extra) {
+export default function createExtraScopePlugin(extra) {
   const scope = `${extra.trim()} `
 
-  return (context, content, selectors, parents, line, column, length, type) => {
+  const extraScopePlugin = (
+    context,
+    content,
+    selectors,
+    parents,
+    line,
+    column,
+    length,
+    type,
+  ) => {
     if (context !== 2 || type === 107) return
 
     for (let i = 0; i < selectors.length; i++) {
       selectors[i] = `${scope}${selectors[i]}`
     }
   }
+
+  return extraScopePlugin
 }
